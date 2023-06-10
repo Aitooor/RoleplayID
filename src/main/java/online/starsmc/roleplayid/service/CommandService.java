d6e367a248dec9fbbcc621fe4a0378c0247d480c
Aitooor <aitorarias19@gmail.com>
2023-06-10 02:37:20 +0200

    feat: added suggestion to commands and keys

--- src/main/java/online/starsmc/roleplayid/service/CommandService.java
+++ src/main/java/online/starsmc/roleplayid/service/CommandService.java
@@ -2,11 +2,23 @@ package online.starsmc.roleplayid.service;
 
 import dev.triumphteam.cmd.bukkit.BukkitCommandManager;
 import dev.triumphteam.cmd.core.BaseCommand;
+import dev.triumphteam.cmd.core.message.MessageKey;
+import dev.triumphteam.cmd.core.suggestion.SuggestionKey;
 import online.starsmc.roleplayid.Main;
+import online.starsmc.roleplayid.util.ChatUtil;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import javax.inject.Inject;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CommandService implements Service{
     @Inject private Set<BaseCommand> commands;
@@ -17,6 +29,9 @@ public class CommandService implements Service{
     public void start() {
         BukkitCommandManager<CommandSender> manager = BukkitCommandManager.create(plugin);
 
+        this.registerSuggestions(manager);
+        this.registerKeys(manager);
+
         commands.forEach(manager::registerCommand);
     }
 
@@ -25,5 +40,40 @@ public class CommandService implements Service{
         BukkitCommandManager<CommandSender> manager = BukkitCommandManager.create(plugin);
 
         commands.forEach(manager::unregisterCommand);
+    }
+
+    public void registerSuggestions(BukkitCommandManager<CommandSender> manager) {
+        manager.registerSuggestion(SuggestionKey.of("players"), (sender, context) -> {
+            List<String> players = new ArrayList<>();
+            Bukkit.getOnlinePlayers().forEach(player -> players.add(player.getName()));
+            return players;
+        });
+        manager.registerSuggestion(SuggestionKey.of("pages"), (sender, context) -> {
+            List<String> pages = new ArrayList<>();
+            pages.add("1");
+            pages.add("2");
+            pages.add("3");
+            pages.add("4");
+            return pages;
+        });
+    }
+
+    public void registerKeys(BukkitCommandManager<CommandSender> manager) {
+        manager.registerMessage(MessageKey.NOT_ENOUGH_ARGUMENTS, (sender, context) -> {
+            ChatUtil.sendMsgPlayer(sender, "");
+            ChatUtil.sendMsgPlayer(sender, "&7Use space and tab key for auto complete.");
+            if(context.getSubCommand().isEmpty()) {
+                ChatUtil.sendMsgPlayer(sender, "&cLike: /" + context.getCommand() + " (args)");
+            } else {
+                ChatUtil.sendMsgPlayer(sender, "&cLike: /" + context.getCommand() + " " + context.getSubCommand() + " (args)");
+            }
+            ChatUtil.sendMsgPlayer(sender, "");
+        });
+        manager.registerMessage(MessageKey.UNKNOWN_COMMAND, (sender, context) -> {
+            ChatUtil.sendMsgPlayer(sender, "");
+            ChatUtil.sendMsgPlayer(sender, "&7Use space and tab key for auto complete.");
+            ChatUtil.sendMsgPlayer(sender, "&cLike: /" + context.getCommand() + " (args)");
+            ChatUtil.sendMsgPlayer(sender, "");
+        });
     }
 }
